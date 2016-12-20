a84f1bbd2b52413c781ba1c082591c19e2d47a8d
Matthew Rollings <matthewrollings@gmail.com>
2016-12-20 20:43:27 +0000

    Added screen waking to tests

--- app/src/androidTest/java/com/stealthcotper/networktools/MainActivityTest.java
+++ app/src/androidTest/java/com/stealthcotper/networktools/MainActivityTest.java
@@ -4,10 +4,9 @@ package com.stealthcotper.networktools;
  * Created by matthew on 20/12/16.
  */
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.WindowManager;
 
 import com.squareup.spoon.Spoon;
 
@@ -16,97 +15,73 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
-import static android.support.test.espresso.intent.matcher.UriMatchers.hasHost;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static android.support.test.espresso.intent.Intents.intended;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
+
+    private MainActivity activity;
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
 
+    @Before
+    public void setUp() {
+        activity = mActivityRule.getActivity();
+
+        // Code to wake up screen before running tests
+        Runnable wakeUpDevice = new Runnable() {
+            public void run() {
+                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            }
+        };
+        activity.runOnUiThread(wakeUpDevice);
+    }
+
     @Test
     public void checkPing() {
-        // Type text and then press the button.
-        String hostName = "google.com";
 
-        // Enter text
-        onView(withId(R.id.editIpAddress))
-                .perform(clearText(), typeText(hostName), closeSoftKeyboard());
-
-        // Check text is entered
-        onView(withId(R.id.editIpAddress)).check(matches(withText(hostName)));
+        setUpText("google.com");
 
         // Click ping button
         onView(withId(R.id.pingButton)).perform(click());
 
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        sleep();
 
         Spoon.screenshot(mActivityRule.getActivity(), "ping");
     }
 
     @Test
     public void checkWOL() {
-        // Type text and then press the button.
-        String hostName = "localhost";
 
-        // Enter text
-        onView(withId(R.id.editIpAddress))
-                .perform(clearText(), typeText(hostName), closeSoftKeyboard());
-
-        // Check text is entered
-        onView(withId(R.id.editIpAddress)).check(matches(withText(hostName)));
+        setUpText("localhost");
 
         // Click ping button
         onView(withId(R.id.wolButton)).perform(click());
 
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        sleep();
 
         Spoon.screenshot(mActivityRule.getActivity(), "wake-on-lan");
     }
 
     @Test
     public void checkPortScan() {
-        // Type text and then press the button.
-        String hostName = "localhost";
-
-        // Enter text
-        onView(withId(R.id.editIpAddress))
-                .perform(clearText(), typeText(hostName), closeSoftKeyboard());
-
-        // Check text is entered
-        onView(withId(R.id.editIpAddress)).check(matches(withText(hostName)));
+        setUpText("localhost");
 
         // Click ping button
         onView(withId(R.id.portScanButton)).perform(click());
 
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        sleep();
 
         Spoon.screenshot(mActivityRule.getActivity(), "port_scan");
     }
@@ -118,4 +93,22 @@ public class MainActivityTest {
 //        intended(allOf(hasData(hasHost(equalTo("www.google.com"))),
 //                hasAction(Intent.ACTION_VIEW)));
 //    }
+
+    private void setUpText(String hostNameOrIp){
+        // Enter text
+        onView(withId(R.id.editIpAddress))
+                .perform(clearText(), typeText(hostNameOrIp), closeSoftKeyboard());
+
+        // Check text is entered
+        onView(withId(R.id.editIpAddress)).check(matches(withText(hostNameOrIp)));
+    }
+
+    private void sleep(){
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
