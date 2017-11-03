3be7beb8104403cab4738ed5c59dfae420d371ae
matthew <matthewrollings@gmail.com>
2017-11-03 13:45:08 +0000

    Changed to md5

--- scripts/decrypt-secrets.sh
+++ scripts/decrypt-secrets.sh
@@ -7,7 +7,7 @@ ls -al .circleci/
 echo $ANDROID_NETWORK_TOOLS_DECRYPTKEY1
 
 # Encrypt
-#openssl aes-256-cbc -md sha256 -e -in key.p12 -out .circleci/key.p12.enc -k $ANDROID_NETWORK_TOOLS_DECRYPTKEY1
+#openssl aes-256-cbc -md md5 -e -in key.p12 -out .circleci/key.p12.enc -k $ANDROID_NETWORK_TOOLS_DECRYPTKEY1
 
 # Decrypt
-openssl aes-256-cbc -md sha256 -d -in .circleci/key.p12.enc -out test_key.p12 -k $ANDROID_NETWORK_TOOLS_DECRYPTKEY1
+openssl aes-256-cbc -md md5 -d -in .circleci/key.p12.enc -out key.p12 -k $ANDROID_NETWORK_TOOLS_DECRYPTKEY1
