96a3014e3f4b599f5d662393ec1f3933d7f6e0d5
matthew <matthewrollings@gmail.com>
2017-11-03 12:46:02 +0000

    Debugging script

--- scripts/decrypt-secrets.sh
+++ scripts/decrypt-secrets.sh
@@ -3,4 +3,7 @@
 # Following this guide to encrypt / decrypt files
 # https://github.com/circleci/encrypted-files
 
+ls -al .circleci/
+echo $ANDROID_NETWORK_TOOLS_DECRYPTKEY1
+
 openssl aes-256-cbc -d -in .circleci/key.p12.enc -out key.p12 -k $ANDROID_NETWORK_TOOLS_DECRYPTKEY1
