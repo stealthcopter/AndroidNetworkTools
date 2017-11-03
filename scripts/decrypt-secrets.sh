907c5da58f1655407f74b6c3373a86fd147de650
matthew <matthewrollings@gmail.com>
2017-11-03 13:24:23 +0000

    Fixed secret keys

--- scripts/decrypt-secrets.sh
+++ scripts/decrypt-secrets.sh
@@ -4,11 +4,10 @@
 # https://github.com/circleci/encrypted-files
 
 ls -al .circleci/
-
 echo $ANDROID_NETWORK_TOOLS_DECRYPTKEY1
 
 # Encrypt
-#openssl aes-256-cbc -e -in key.p12 -out .circleci/key.p12.enc -k "${ANDROID_NETWORK_TOOLS_DECRYPTKEY1}"
+#openssl aes-256-cbc -e -in key.p12 -out .circleci/key.p12.enc -k $ANDROID_NETWORK_TOOLS_DECRYPTKEY1
 
 # Decrypt
-openssl aes-256-cbc -d -in .circleci/key.p12.enc -out key.p12 -k "${ANDROID_NETWORK_TOOLS_DECRYPTKEY1}"
+openssl aes-256-cbc -d -in .circleci/key.p12.enc -out key.p12 -k $ANDROID_NETWORK_TOOLS_DECRYPTKEY1
